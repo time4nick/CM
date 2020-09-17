fab3097b2060ed677d06b49ff520ebe5b256be04
nick <iracrand@gmail.com>
2020-09-17 17:43:59 -0500

    nav bar1

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+    $('.btn').click(function () {
+        $('.nav-links').slideToggle(1000);
+        $('.btn').toggleClass('change');
+    })
+})
